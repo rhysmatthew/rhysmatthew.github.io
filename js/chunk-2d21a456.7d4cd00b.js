4b9003f0e97f4dc3e5f725df9259e962ab2efb87
Rhys <rhys@rhys-mac.local>
2021-04-12 00:42:24 +0100

    v1.2.3 - Intro animation, increase duration

--- js/chunk-2d21a456.7d4cd00b.js
+++ js/chunk-2d21a456.7d4cd00b.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a456"],{bb99:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"container"},[e("vue-headful",{attrs:{title:"Page not found | Rhys Matthew"}}),e("section",{staticClass:"section text--center"},[e("h1",{staticClass:"h2"},[t._v("Page not found")]),e("p",[t._v("That page could not be found. "),e("router-link",{staticClass:"btn",attrs:{exact:"",to:"/"}},[e("span",{staticClass:"btn__inner"},[t._v("Return home")])])],1)])],1)},s=[],c=e("2877"),o={},i=Object(c["a"])(o,a,s,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d21a456.790b29d8.js.map
+//# sourceMappingURL=chunk-2d21a456.7d4cd00b.js.map
